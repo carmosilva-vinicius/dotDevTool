9428af9358c649975cb5cc253a90c5848cb25f23
agrosuredev <carmosilva.vinicius@gmail.com>
2023-03-08 16:51:08 -0300

    Start including command parse

---
+++ src/Models/ProjectConfig.cs
@@ -0,0 +1,11 @@
+
+using dotDevTool.src.Models.types;
+
+namespace dotDevTool.src.Models
+{
+    public class ProjectConfig
+    {
+        public string ProjectName { get; set; }
+        public Database Database { get; set; }
+    }
+}
